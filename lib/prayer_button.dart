67c3bfb4bd161081eef4eafe2d3c1eb6d96c6676
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2025-10-09 18:38:38 +0300

    Finish PrayerButton widget

--- lib/prayer_button.dart
+++ lib/prayer_button.dart
@@ -1,25 +1,83 @@
-import 'package:flutter/widgets.dart';
+// ignore: unused_import
+import 'dart:developer';
 
-enum ButtonState { empty, half, full }
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:prayertracker/main.dart';
+
+enum PrayerValue {
+  empty(0, Colors.white, Colors.grey),
+  half(1, Colors.grey, Colors.grey),
+  full(2, Colors.black, Colors.black);
+
+  final int value;
+  final Color colorValue;
+  final Color borderColor;
+  const PrayerValue(this.value, this.colorValue, this.borderColor);
+}
 
 enum ButtonType { morning, noon, afternoon, night, evening }
 
-class Prayerbutton extends StatelessWidget {
-  late final ButtonType _buttonType;
-  late final ButtonState _state;
+class PrayerButton extends ConsumerStatefulWidget {
+  final ButtonType buttonType;
+  final PrayerValue initialButtonState;
+  final String buttonID;
 
-  Prayerbutton({super.key, required ButtonType buttonType, ButtonState state = ButtonState.empty}) {
-    _buttonType = buttonType;
-    _state = state;
+  const PrayerButton({
+    super.key,
+    required this.buttonID,
+    required this.buttonType,
+    this.initialButtonState = PrayerValue.empty,
+  });
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _PrayerButtonState();
+}
+
+class _PrayerButtonState extends ConsumerState<PrayerButton> {
+  @override
+  void initState() {
+    log('Creating ${widget.buttonID}', name: 'UI');
+
+    ref.read(buttonStates.notifier).state[widget.buttonID] = widget.initialButtonState.value;
+    super.initState();
   }
-
-  /// Forwards the button's state.
-  ///
-  /// Order: empty -> half -> full
-  void forward() {}
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    log('Building ${widget.buttonID}', name: 'UI');
+
+    final currentButtonStateValue = ref.watch(
+      buttonStates.select((value) => value[widget.buttonID]),
+    )!;
+    final currentButtonState = PrayerValue.values[currentButtonStateValue % 3];
+
+    return GestureDetector(
+      onTap: () {
+        final PrayerValue newButtonState = PrayerValue.values[(currentButtonStateValue + 1) % 3];
+        ref.read(buttonStates.notifier).update((state) {
+          return {...state, widget.buttonID: newButtonState.value};
+        });
+
+        HapticFeedback.lightImpact();
+
+        log("Updated ${widget.buttonID}'s state to ${newButtonState.name}", name: 'UI');
+      },
+
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 100),
+          width: 32,
+          height: 32,
+          decoration: BoxDecoration(
+            color: currentButtonState.colorValue,
+            shape: BoxShape.circle,
+            border: BoxBorder.all(color: currentButtonState.borderColor, width: 2),
+          ),
+        ),
+      ),
+    );
   }
 }
