681c3d3b7cfe3b79b595f858b962bbe54ed506e7
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2025-10-09 18:38:44 +0300

    Add dummy buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:prayertracker/prayer_button.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:prayertracker/date_row.dart';
+import 'package:prayertracker/header_row.dart';
+
+final buttonStates = StateProvider<Map<String, int>>((ref) => {});
 
 void main() {
   runApp(ProviderScope(child: const IOSapp()));
@@ -22,5 +26,22 @@ class IOSapp extends StatelessWidget {
 }
 
 CupertinoPageScaffold homePage() {
-  return CupertinoPageScaffold(child: Center(child: Text('Hi')));
+  return CupertinoPageScaffold(
+    child: SafeArea(
+      child: Column(
+        children: [
+          HeaderRow(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 60,
+              itemBuilder: (context, index) {
+                final date = DateTime(2025, 10, 4).subtract(Duration(days: index));
+                return DateRow(date: date);
+              },
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
